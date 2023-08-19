0fbbf25bfaf16dfed18eb6a3e23f0ed505056c44
hardikvaniya <hardikvaniya3800@gmail.com>
2023-08-19 15:42:40 -0700

    update getDaysInMonth file to handle the corner case of leap year

--- getDaysInMonth.js
+++ getDaysInMonth.js
@@ -1,5 +1,13 @@
 export function getDaysInMonth(month, year) {
   const daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  
+  // check if the month of febuary is a leap year
+  if (month === 2 && isLeapYear(year)){
+    return 29;
+  }
+  return daysInMonths[month - 1]; // -1 because the month array is zero indexed
+}
 
-  return daysInMonths[month];
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 }
