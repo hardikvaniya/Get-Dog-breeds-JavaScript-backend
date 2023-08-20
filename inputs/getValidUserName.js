8a2dad69493035c0dfb640e6a0f482a5bc08a05c
hardikvaniya <hardikvaniya3800@gmail.com>
2023-08-19 17:32:03 -0700

    fix the issue of unfefined usernames. add title and values

--- inputs/getValidUserName.js
+++ inputs/getValidUserName.js
@@ -1,10 +1,13 @@
 import { friends } from "../friend-collections.js";
-import { select } from "@inquirer/prompts";
+import { select } from "@inquirer/prompts"; // inquirer is a package to get user input
 
+
+// to selec the user name from the choices
 export async function getValidUserName() {
   const validFriendNames = friends.map(friend => friend.name);
-  const nameChoices = validFriendNames.map(username => ({ title: username, value: username }));
-  const response = await select({
+  const nameChoices = validFriendNames.map(username => ({ title: username, value: username })); // to display the name of the user
+  
+  const response = await select({  // to select the user name
     type: "select",
     name: "username",
     message: "Who are you?",
@@ -12,6 +15,4 @@ export async function getValidUserName() {
   });
 
   return response;
-  
 }
-
